50fddddee063e33f1c784614e22a4b928297d786
slu <slu@cf844efb-b82e-4d8a-941a-17fca4a4ce76>
2016-05-20 10:39:23 +0000

    implementing ignite cache for improved performance
    git-svn-id: svn+ssh://code-01.kb.dk/home/svn/repos/guaman-poma@31 cf844efb-b82e-4d8a-941a-17fca4a4ce76

---
+++ trunk/src/main/java/dk/kb/cache/Page.java
@@ -0,0 +1,55 @@
+package dk.kb.cache;
+
+import javax.cache.*;
+import javax.cache.configuration.*;
+import javax.cache.Caching;
+import javax.cache.CacheManager;
+import javax.cache.Cache;
+import javax.cache.expiry.*;
+import javax.cache.spi.*;
+import static javax.cache.expiry.Duration.ONE_HOUR;
+import static javax.cache.expiry.Duration.ONE_MINUTE;
+import static javax.cache.expiry.Duration.ZERO;
+
+public class Page
+{
+
+    private static Page page    = null;
+
+    Cache<String, String> cache = null;
+    //Cache cache = null;
+
+    private Page() {
+	//resolve a cache manager
+	CachingProvider cachingProvider = Caching.getCachingProvider();
+	CacheManager cacheManager = cachingProvider.getCacheManager();
+
+	MutableConfiguration<String, String> config = new MutableConfiguration<>();
+        config.setStoreByValue(true)
+	    .setExpiryPolicyFactory(AccessedExpiryPolicy.factoryOf(ONE_HOUR))
+	    .setStatisticsEnabled(true);
+
+	//create the cache
+	this.cache = cacheManager.createCache("simpleCache", config);
+    }
+
+    public static Page getInstance() {
+	if(page == null) {
+	    page = new Page();
+	}
+	return page;
+    }
+
+    public String getText(String key) {
+	String val = this.cache.get(key);
+	if(val != null ) {
+	    return val;
+	}
+	return null;
+    }
+
+    public void setText(String key,String doc) {
+	this.cache.put(key, doc);
+    }
+
+}
